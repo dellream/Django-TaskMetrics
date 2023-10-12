ed81af951ca0eff85822f876ec07b01d8c4991ee
dellream <yakimov.as@mail.ru>
2023-10-12 14:07:37 +0500

    Маленький тест для core

--- app/core/tests.py
+++ app/core/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_home_page_uses_correct_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
