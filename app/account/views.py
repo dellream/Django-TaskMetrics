9f71e79d2f9f4532248b01aa253638769dbf197e
dellream <yakimov.as@mail.ru>
2023-10-15 14:10:50 +0500

    Функция для регистрации пользователей

--- app/account/views.py
+++ app/account/views.py
@@ -1,40 +1,24 @@
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
 from django.shortcuts import render
+from .forms import UserRegistrationForm
 
-from .forms import LoginForm
 
-
-def user_login(request):
+def user_register(request):
     """
-    Представление входа пользователя в систему
+    Регистрация пользователей
     """
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-
-        # Если данные введены правильно, считываем данные
-        if not form.is_valid():
-            form = LoginForm()
-        else:
-            cd = form.cleaned_data
-            user = authenticate(
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            service_user_register(user_form)
+            return render(
                 request,
-                username=cd['username'],
-                password=cd['password']
+                'registration/register_done.html',
+                {'new_user': new_user}
             )
-
-            # Проверка, существует ли пользователь
-            if user is None:
-                return HttpResponse('Неправильный логин или пароль')
-            else:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Аутентификация прошла успешно')
-                else:
-                    return HttpResponse('Отключенная учетная запись')
-
-        return render(
-            request,
-            'account/login.html',
-            {'form': form}
-        )
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request,
+        'registration/register.html',
+        {'user_form': user_form}
+    )
