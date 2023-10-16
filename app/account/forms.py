9da5d685dd0a0aad0e8a7354f94e3b9a5c7bd831
dellream <yakimov.as@mail.ru>
2023-10-16 13:08:48 +0500

    Формы для регистрации и изменения пользователя, изменения профиля

--- app/account/forms.py
+++ app/account/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from .models import Profile
+
 
 # class LoginForm(forms.Form):
 #     username = forms.CharField()
@@ -36,3 +38,57 @@ class UserRegistrationForm(forms.ModelForm):
         if User.objects.filter(email=data).exists():
             raise forms.ValidationError('Эта электронная почта уже используется.')
         return data
+
+
+class UserEditForm(forms.ModelForm):
+    """
+    Форма изменения пользователя.
+    """
+    def clean_email(self):
+        """
+        Проверяет есть ли предоставляемый email в БД. Если есть, то райзит ошибку,
+        иначе возвращает почту
+        """
+        email = self.cleaned_data['email']
+        queryset = User.objects.filter(email=email).exclude(id=self.instance.id)
+        if queryset.exists():
+            raise forms.ValidationError('Эта электронная почта уже используется')
+        return email
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'email'
+        ]
+        labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'email': 'Электронная почта',
+        }
+
+
+class ProfileEditForm(forms.ModelForm):
+    """
+    Форма изменения профиля
+    """
+    birth_date = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        label='Дата рождения'
+    )
+
+    class Meta:
+        model = Profile
+        fields = [
+            'birth_date',
+            'avatar',
+            'bio',
+            'slug'
+        ]
+        labels = {
+            'avatar': 'Фотография',
+            'birth_date': 'Дата рождения',
+            'bio': 'Информация о себе',
+            'slug': 'Ссылка на профиль'
+        }
