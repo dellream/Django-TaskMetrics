1d3aa1b10af8d20c796686e2a663e3d94bbff152
dellream <yakimov.as@mail.ru>
2023-10-16 13:07:59 +0500

    Вынесение логики в сервисный слой, реализация изменения профиля и пользователя

--- app/account/services/services.py
+++ app/account/services/services.py
@@ -1,16 +1,71 @@
 """
 Сервисный слой для вынесения бизнес-логики из представлений (views)
 """
-from app.account.models import Profile
+from ..models import Profile
+from ..forms import (
+    UserRegistrationForm,
+    UserEditForm,
+    ProfileEditForm
+)
+from django.contrib import messages
 
 
 def service_user_register(user_form):
     """
-    Функция для регистрации пользователя
+    Регистрация пользователя и создание для него профиля
+    Возвращает new_user, если форма валидна, иначе None
     """
-    new_user = user_form.save(commit=False)
-    new_user.set_password(user_form.cleaned_data['password'])
-    new_user.save()
-    Profile.objects.create(user=new_user)
+    if user_form.is_valid():
+        new_user = user_form.save(commit=False)
+        new_user.set_password(user_form.cleaned_data['password'])
+        new_user.save()
+        Profile.objects.create(user=new_user)
+        return new_user
+    return None
 
-    return new_user
+
+def service_create_user_registration_from(request):
+    """
+    Создание формы регистрации
+    Возвращает форму регистрации пользователя и new_user,
+    если пользователь новый, если существующий, то
+    возвращает форму и None
+    """
+    if not request.method == 'POST':
+        user_form = UserRegistrationForm()
+    else:
+        user_form = UserRegistrationForm(request.POST)
+        new_user = service_user_register(user_form)
+        if new_user:
+            return user_form, new_user
+
+    return user_form, None
+
+
+def service_edit_user_and_profile(request):
+    """
+    Редактирование пользователя и его профиля
+    """
+    if not request.method == 'POST':
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    else:
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+            messages.success(request, 'Профиль успешно изменен')
+        else:
+            messages.error(request, 'Ошибка изменения профиля')
+
+    return user_form, profile_form
--- app/account/views.py
+++ app/account/views.py
@@ -1,26 +1,49 @@
 from django.shortcuts import render
 
-from .services.services import service_user_register
-from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+
+from .services.services import (
+    service_user_register,
+    service_edit_user_and_profile,
+    service_create_user_registration_from,
+)
 
 
 def user_register(request):
     """
     Регистрация пользователей
     """
-    if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            new_user = service_user_register(user_form)
-            return render(
-                request,
-                'registration/register_done.html',
-                {'new_user': new_user}
-            )
-    else:
-        user_form = UserRegistrationForm()
+    # Вызываем функцию для создания формы
+    user_form, new_user = service_create_user_registration_from(request)
+
+    # Если создаем нового пользователя:
+    if new_user:
+        return render(
+            request,
+            'registration/register_done.html',
+            {'new_user': new_user}
+        )
+
+    # Если создаваемый пользователь уже существует:
     return render(
         request,
         'registration/register.html',
         {'user_form': user_form}
+    )
+
+
+@login_required
+def edit_user_and_profile(request):
+    """
+    Редактирование пользователя и его профиля
+    """
+    user_form, profile_form = service_edit_user_and_profile(request)
+
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form
+        }
     )
