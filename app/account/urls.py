815b50db4536e8c3a80acb3565e009c172390160
dellream <yakimov.as@mail.ru>
2023-10-17 11:09:08 +0500

    Исправление шаблонов и пути register

--- app/account/urls.py
+++ app/account/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     # Работа с пользователями
     # Регистрация пользователя
-    path('register/', views.user_register, name='user_register'),
+    path('register/', views.user_register, name='register'),
     # Редактирование пользователя
     path('edit/', views.edit_user_and_profile, name='edit_user_and_profile'),
 ]
