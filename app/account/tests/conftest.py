80dcca44196350c4a02677b5c4f264795b085f36
dellream <yakimov.as@mail.ru>
2023-10-19 11:59:17 +0500

    Фикстуры для тестов account

--- app/account/tests/conftest.py
+++ app/account/tests/conftest.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.test import Client
 
 
-
 @pytest.fixture
 def test_user():
     return User.objects.create_user(username='testuser', password='testpassword')
