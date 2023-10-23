967c519a0e44641524e76848b5fc44db783cfc83
dellream <yakimov.as@mail.ru>
2023-10-23 15:24:51 +0500

    Изменение тестов

--- app/account/tests/conftest.py
+++ app/account/tests/conftest.py
@@ -1,6 +1,19 @@
 import pytest
 from django.contrib.auth.models import User
 from django.test import Client
+from selenium import webdriver
+
+
+@pytest.fixture(scope='class')
+def browser():
+    """
+    Фикстура для получения экземпляра драйвера для Хрома
+    """
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    driver.quit()
 
 
 @pytest.fixture
--- app/account/tests/test_auth.py
+++ app/account/tests/test_auth.py
@@ -1,26 +1,34 @@
+import time
+
 import pytest
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.test import Client
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
+@pytest.mark.usefixtures('browser')
 class TestBasicUserAuth:
-    def test_home_page(self, client):
-        """Проверка домашней страницы"""
+    def test_home_page(self, client: Client) -> None:
+        """Вход на домашнюю страницу"""
         response = client.get(reverse('core:home'))
         assert response.status_code == 200
 
-    @pytest.mark.django_db
-    def test_logout(self, client, test_user):
-        """Проверка выхода из системы"""
-        client.login(username='testuser', password='testpassword')
-        response = client.get(reverse('logged_out'))
-        assert response.status_code == 200
-
-    def test_register(self, client):
+    def test_register(
+            self,
+            client: Client,
+            browser
+    ) -> None:
         """
         Проверка регистрации пользователя
         через форму регистрации
         """
+        browser.get(reverse('core:home'))
+        time.sleep(10)
         response = client.get(reverse('register'))
         assert response.status_code == 200
         # Регистрация нового пользователя
@@ -37,15 +45,22 @@ class TestBasicUserAuth:
         # Проверки для авторизованного пользователя
 
     @pytest.mark.django_db
-    def test_logout_after_login(self, client, test_user):
-        """
-        Проверка выхода из системы через интерфейс
-        после авторизации
-        """
-        client.login(username='testuser', password='testpassword')
-        response = client.get(reverse('logged_out'))
-        assert response.status_code == 200
-        # Проверки для выхода после авторизации
+    def test_logout(
+            self,
+            client: Client,
+            browser
+    ) -> None:
+        """Выход из системы авторизованного пользователя"""
+        if client.is_authenticated:
+            browser.get(reverse('core:home'))
+            print('test_logout: Получена домашняя страница')
+
+            home_logout_button = browser.find_element(By.CLASS_NAME, 'logout_logo')
+            home_logout_button.click()
+            print('test_logout: Выполнен клик на изображение выхода из системы')
+
+            response = client.get(reverse('logged_out'))
+            assert response.status_code == 200
 
     @pytest.mark.django_db
     def test_delete_user(self, test_user):
