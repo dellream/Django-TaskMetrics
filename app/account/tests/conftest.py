21bfe9903aeca7f7f1f600c3124a0bf881062888
dellream <yakimov.as@mail.ru>
2023-10-24 13:39:11 +0500

    Фикстура для обращения к основной БД во время тестов

--- app/account/tests/conftest.py
+++ app/account/tests/conftest.py
@@ -1,7 +1,11 @@
 import pytest
 from django.contrib.auth.models import User
+from django.test import modify_settings
 from django.test import Client
 from selenium import webdriver
+
+from app.config import settings
+from app.config.conf import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 
 
 @pytest.fixture(scope='class')
@@ -10,17 +14,30 @@ def browser():
     Фикстура для получения экземпляра драйвера для Хрома
     """
     options = webdriver.ChromeOptions()
+    service = webdriver.ChromeService(
+        executable_path='/home/alexey/prog/Django-TaskMetrics/app/config/chromeDriver/chromedriver'
+    )
     # options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options, service=service)
     yield driver
     driver.quit()
 
 
 @pytest.fixture
-def test_user():
-    return User.objects.create_user(username='testuser', password='testpassword')
-
-
-@pytest.fixture
 def client():
     return Client()
+
+
+@pytest.fixture(scope='session')
+def django_db_setup():
+    """
+    Фисктура для обращения к основной базе данных
+    """
+    settings.DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+    }
