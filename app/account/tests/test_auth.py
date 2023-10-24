69d0f538daf19ce952562a7efa8202a80605fdbf
dellream <yakimov.as@mail.ru>
2023-10-24 13:42:13 +0500

    Тест регистрации пользователя с помощью интерфейса

--- app/account/tests/test_auth.py
+++ app/account/tests/test_auth.py
@@ -10,6 +10,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from app.config.conf import HOME_URL
+
 
 @pytest.mark.usefixtures('browser')
 class TestBasicUserAuth:
@@ -17,7 +19,9 @@ class TestBasicUserAuth:
         """Вход на домашнюю страницу"""
         response = client.get(reverse('core:home'))
         assert response.status_code == 200
+        print('\ntest_home_page: Получена домашняя страница')
 
+    @pytest.mark.django_db
     def test_register(
             self,
             client: Client,
@@ -27,44 +31,79 @@ class TestBasicUserAuth:
         Проверка регистрации пользователя
         через форму регистрации
         """
-        browser.get(reverse('core:home'))
-        time.sleep(10)
+        browser.get(HOME_URL)
+        print('\ntest_register: Получена домашняя страница')
+        response = client.get(HOME_URL)
+        assert response.status_code == 200
+
+        # Регистрация нового пользователя
+        register_button = browser.find_element(By.CLASS_NAME, 'register')
+        register_button.click()
         response = client.get(reverse('register'))
         assert response.status_code == 200
-        # Регистрация нового пользователя
+        print('test_register: Переход на страницу регистрации')
 
-    @pytest.mark.django_db
-    def test_login(self, client, test_user):
-        """
-        Проверка авторизации зарегистрированного
-        пользователя через форму авторизации
-        """
-        client.login(username='testuser', password='testpassword')
-        response = client.get(reverse('core:home'))
-        assert response.status_code == 200
-        # Проверки для авторизованного пользователя
+        # Поиск полей для регистрации
+        username_input = browser.find_element(By.ID, 'id_username')
+        first_name_input = browser.find_element(By.ID, 'id_first_name')
+        email_input = browser.find_element(By.ID, 'id_email')
+        password_input = browser.find_element(By.ID, 'id_password')
+        confirm_password_input = browser.find_element(By.ID, 'id_password2')
 
-    @pytest.mark.django_db
-    def test_logout(
-            self,
-            client: Client,
-            browser
-    ) -> None:
-        """Выход из системы авторизованного пользователя"""
-        if client.is_authenticated:
-            browser.get(reverse('core:home'))
-            print('test_logout: Получена домашняя страница')
+        # Внесение данных в поля
+        username_input.send_keys('test_register_username')
+        first_name_input.send_keys('test_register_first_name')
+        email_input.send_keys('test_register_email@test.test')
+        password_input.send_keys('test_register_password')
+        confirm_password_input.send_keys('test_register_password')
+        # time.sleep(3)
 
-            home_logout_button = browser.find_element(By.CLASS_NAME, 'logout_logo')
-            home_logout_button.click()
-            print('test_logout: Выполнен клик на изображение выхода из системы')
+        create_acc_button = browser.find_element(By.XPATH, '//input[@value="Создать мой аккаунт"]')
+        create_acc_button.click()
 
-            response = client.get(reverse('logged_out'))
-            assert response.status_code == 200
+        # Проверка, создан ли такой пользователь
+        user = User.objects.get(username='test_register_username')
+        print(f'test_register: Пользователь "test_register_username" найден в БД')
+        assert user.username == 'test_register_username'
 
-    @pytest.mark.django_db
-    def test_delete_user(self, test_user):
-        user_to_delete = User.objects.get(username='testuser')
-        user_to_delete.delete()
-        assert User.objects.filter(username='testuser').count() == 0
-        # Проверки для удаления пользователя
+        # Тут нужно обратиться к основной БД, а не тестовой и проверить, появился ли пользователь с username
+        # test_register_username, в админке он появляется, то есть код правильный, как это в тест загнать?
+
+    print('\ntest_register: Регистрация завершена')
+
+
+    # @pytest.mark.django_db
+    # def test_login(self, client, test_user):
+    #     """
+    #     Проверка авторизации зарегистрированного
+    #     пользователя через форму авторизации
+    #     """
+    #     client.login(username='testuser', password='testpassword')
+    #     response = client.get(reverse('core:home'))
+    #     assert response.status_code == 200
+    #     # Проверки для авторизованного пользователя
+    #
+    # @pytest.mark.django_db
+    # def test_logout(
+    #         self,
+    #         client: Client,
+    #         browser
+    # ) -> None:
+    #     """Выход из системы авторизованного пользователя"""
+    #     if client.is_authenticated:
+    #         browser.get(reverse('core:home'))
+    #         print('test_logout: Получена домашняя страница')
+    #
+    #         home_logout_button = browser.find_element(By.CLASS_NAME, 'logout_logo')
+    #         home_logout_button.click()
+    #         print('test_logout: Выполнен клик на изображение выхода из системы')
+    #
+    #         response = client.get(reverse('logged_out'))
+    #         assert response.status_code == 200
+    #
+    # @pytest.mark.django_db
+    # def test_delete_user(self, test_user):
+    #     user_to_delete = User.objects.get(username='testuser')
+    #     user_to_delete.delete()
+    #     assert User.objects.filter(username='testuser').count() == 0
+    #     # Проверки для удаления пользователя
