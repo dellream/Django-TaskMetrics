54c8f9009836dcf2113854d96df7b82edf2922fc
dellream <yakimov.as@mail.ru>
2023-10-26 11:50:56 +0500

    Исправление test_logout для корректного запуска через терминал

--- app/account/tests/test_auth.py
+++ app/account/tests/test_auth.py
@@ -18,8 +18,9 @@ class TestBasicUserAuth:
         """Вход на домашнюю страницу"""
         response = client.get(reverse('core:home'))
         assert response.status_code == 200
-        print('\ntest_home_page: Получена домашняя страница')
+        print('\ntest_home_page: Успешно')
 
+    @pytest.mark.ordering(1)
     @pytest.mark.django_db
     def test_register(self, driver) -> None:
         """
@@ -67,11 +68,9 @@ class TestBasicUserAuth:
             assert user.username == 'test_register_username'
             print(f'test_register: Пользователь "test_register_username" найден в БД')
 
-            # Тут нужно обратиться к основной БД, а не тестовой и проверить, появился ли пользователь с username
-            # test_register_username, в админке он появляется, то есть код правильный, как это в тест загнать?
+            print('test_register: Успешно')
 
-            print('\ntest_register: Регистрация завершена')
-
+    @pytest.mark.ordering(2)
     @pytest.mark.django_db
     def test_login(self, driver) -> None:
         """
@@ -104,6 +103,7 @@ class TestBasicUserAuth:
         assert driver.current_url == HOME_URL
         print('test_login: Редирект после авторизации произошел')
 
+    @pytest.mark.ordering(3)
     @pytest.mark.django_db
     def test_logout(
             self,
@@ -111,8 +111,10 @@ class TestBasicUserAuth:
             driver
     ) -> None:
         """Выход из системы авторизованного пользователя"""
-        self.test_login(driver)
+        # Если нужно запустить один тест отдельно, то нужно сначала залогиниться
+        # self.test_login(driver)
         driver.get(HOME_URL)
+        time.sleep(5)
         print('test_logout: Получена домашняя страница')
 
         home_logout_button = driver.find_element(By.CLASS_NAME, 'logout_logo')
@@ -121,4 +123,9 @@ class TestBasicUserAuth:
 
         # Проверяем, что пользователь больше не авторизован
         assert '_auth_user_id' not in client.session
-        print('test_logout: Пользователь успешно вышел из системы')
+        print('test_logout: Успешно')
+
+    # def test_forgot_password(self):
+    #     """
+    #     Тест для проверки отправки почты при заполнении формы 'Забыли пароль'
+    #     """
