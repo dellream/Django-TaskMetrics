809d03b573f2c2a3257fd72dafba4ece7d509695
dellream <yakimov.as@mail.ru>
2023-11-03 12:32:42 +0500

    Добавление моделей в админ-панель

--- app/courses/admin.py
+++ app/courses/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Subject, Course, Module
 
-# Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    """
+    Административная панель для модели Subject.
+    """
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    """
+    Встроенная административная панель для модели Module.
+    """
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    """
+    Административная панель для модели Course.
+    """
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
