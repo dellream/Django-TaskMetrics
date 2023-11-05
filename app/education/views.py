0a4b96af6d89a4b7a91ce3c202197415b38113c2
dellream <yakimov.as@mail.ru>
2023-11-05 10:54:12 +0500

    Представления на основе классов с использованием миксинов для CRUD операций с курсами

--- app/education/views.py
+++ app/education/views.py
@@ -1,3 +1,97 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Course
 
-# Create your views here.
+
+class OwnerMixin:
+    """
+    Миксин для фильтрации QuerySet'а по автору.
+
+    Примесный класс OwnerMixin можно использовать в представлениях,
+    которые взаимодействуют с любой моделью, содержащей атрибут owner.
+    """
+
+    def get_queryset(self):
+        """Получает QuerySet, отфильтрованный по текущему пользователю."""
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+    """
+    Миксин для назначения автора объекта.
+    """
+    def form_valid(self, form):
+        """Назначает текущего пользователя в качестве автора."""
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin):
+    """
+    Базовый миксин для управления курсами, связанными с автором.
+    """
+    model = Course
+    fields = [
+        'subject',
+        'title',
+        'slug',
+        'overview'
+    ]
+    success_urls = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    """
+    Миксин для редактирования курсов, связанных с автором.
+    """
+    template_name = 'manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    """
+    Представление для отображения списка курсов автора.
+
+    Указанное представление определяет специальный атрибут template_name для шаблона,
+    который будет выводить список курсов;
+    """
+    template_name = 'manage/course/list.html'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    """
+    Представление для создания нового курса автора.
+
+    В указанном представлении используются поля, определенные в примесном
+    классе OwnerCourseMixin, чтобы компоновать модельную форму;
+
+    Это представление также является подклассом класса CreateView.
+    Оно использует шаблон, определенный в примесном классе OwnerCourseEditMixin;
+    """
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    """
+    Представление для обновления курса автора.
+
+    Обеспечивает возможность редактировать существующий объект Course.
+
+    В указанном представлении используются поля, определенные в примесном
+    классе OwnerCourseMixin, чтобы компоновать модельную форму;
+
+    Это представление также является подклассом класса UpdateView.
+    Оно использует шаблон, определенный в примесном классе OwnerCourseEditMixin;
+    """
+    pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    """
+    Представление для удаления курса автора.
+
+    В указанном представлении определяется специальный атрибут
+    template_name для шаблона, который будет подтверждать удаление курса.
+    """
+    template_name = 'manage/course/delete.html'
