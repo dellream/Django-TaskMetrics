b8fe0e1fa1e61403f2bb40367c4926c0352303b1
dellream <yakimov.as@mail.ru>
2023-11-17 17:46:59 +0500

    Вывод в консоль уровня INFO

--- app/config/logging_config.py
+++ app/config/logging_config.py
@@ -4,9 +4,9 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
-      'standart': {
-          'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
-      },
+        'standart': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
+        },
     },
     'handlers': {
         'rotating_file': {
@@ -15,15 +15,17 @@ LOGGING = {
             'formatter': 'standart',
             'maxBytes': 1024 * 1024 * 100,  # 100 MB
             'backupCount': 10,
+            'level': 'WARNING',  # Установите уровень на WARNING, чтобы записывать только WARNING и выше
         },
         'console': {
             'class': 'logging.StreamHandler',
             'formatter': 'standart',
+            'level': 'INFO',  # Установите уровень на DEBUG, чтобы выводить все уровни в консоль
         },
     },
     'root': {
         'handlers': ['rotating_file', 'console'],
-        'level': 'WARNING',
+        'level': 'INFO',
     },
 }
 
